b84d18124808fef704a9eb60fcacfd2be2ca2d75
Tobias Hecker <heckertobias@googlemail.com>
2025-06-29 14:02:41 +0000

    add tailwindcss to project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,8 @@ export const Route = createFileRoute('/')({
 
 function Index() {
   return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
+    <div>
+      <h3 className="text-3xl font-bold underline">Welcome Home!</h3>
     </div>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    // Please make sure that '@tanstack/router-plugin' is passed before '@vitejs/plugin-react'
+    tailwindcss(),
     tanstackRouter({
       target: 'react',
       autoCodeSplitting: true,
